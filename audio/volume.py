4ab65bd3030dc8dc42d99ab131aa51b7166a116f
Elia Giacobazzi <elia.giacobazzi@gmail.com>
2024-07-03 04:02:30 +0200

    Use pyaudio and pyalsaaudio to handle volume change sound

--- audio/volume.py
+++ audio/volume.py
@@ -1,61 +1,50 @@
 import subprocess
 import time
+import wave
 from pathlib import Path
 
+import alsaaudio as aa
 import keyboard
+import pyaudio
 
 from . import settings
 
-CARD_ID = 1
-STEP = 5
+
+def reproduce_sound(sound_path: Path = settings.SOUND_VOLUME_CHANGE):
+    with wave.open(str(sound_path), "rb") as wf:
+        p = pyaudio.PyAudio()
+
+        stream = p.open(
+            format=p.get_format_from_width(wf.getsampwidth()),
+            channels=wf.getnchannels(),
+            rate=wf.getframerate(),
+            output=True,
+        )
+
+        while len(data := wf.readframes(1024)):
+            stream.write(data)
+
+        stream.close()
+        p.terminate()
 
 
-def on_volume_up():
-    subprocess.Popen(
-        [
-            "amixer",
-            "sset",
-            "-c",
-            f"{settings.CARD_ID}",
-            settings.DEVICE_ID,
-            f"{settings.VOLUME_STEP}%+",
-        ]
-    )
-    subprocess.Popen(
-        [
-            "aplay",
-            "--device",
-            settings.DEVICE_FANCY_ID,
-            str(settings.SOUND_VOLUME_CHANGE),
-        ]
-    )
+def increase_volume(mixer: aa.Mixer, step: int = settings.VOLUME_STEP):
+    for i, current in enumerate(mixer.getvolume()):
+        mixer.setvolume(min(100, current + step), channel=i)
+    reproduce_sound()
 
 
-def on_volume_down():
-    subprocess.Popen(
-        [
-            "amixer",
-            "sset",
-            "-c",
-            f"{settings.CARD_ID}",
-            settings.DEVICE_ID,
-            f"{settings.VOLUME_STEP}%-",
-        ]
-    )
-    subprocess.Popen(
-        [
-            "aplay",
-            "--device",
-            settings.DEVICE_FANCY_ID,
-            str(settings.SOUND_VOLUME_CHANGE),
-        ]
-    )
-
-
-keyboard.add_hotkey(115, on_volume_up)
-keyboard.add_hotkey(114, on_volume_down)
+def decrease_volume(mixer: aa.Mixer, step: int = settings.VOLUME_STEP):
+    for i, current in enumerate(mixer.getvolume()):
+        mixer.setvolume(max(0, current - step), channel=i)
+    reproduce_sound()
 
 
 if __name__ == "__main__":
+    mixer = aa.Mixer(cardindex=settings.MIXER_CARD, control=settings.MIXER_CONTROL)
+
+    keyboard.add_hotkey(115, lambda: increase_volume(mixer))
+    keyboard.add_hotkey(114, lambda: decrease_volume(mixer))
+
     while True:
         time.sleep(0.1)
