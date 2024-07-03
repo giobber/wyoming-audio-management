37ec8cd05f8168acd341df98b2954313cbf584b2
Elia Giacobazzi <elia.giacobazzi@gmail.com>
2024-07-03 04:01:54 +0200

    improvements and move to pyalsaaudio

--- audio/attenuation.py
+++ audio/attenuation.py
@@ -1,22 +1,22 @@
-import astral
 import datetime as dt
-import pytz
 
+import alsaaudio as aa
+import astral
+import pytz
 from astral.sun import sun
 
-location = astral.LocationInfo("Sassuolo", "Italy", "Europe/Rome", 44.55, 10.78)
+from . import settings
+
+
+def attenuate(mixer: aa.Mixer, percentage: float = 0.25):
+    current = mixer.getvolume()
+    mixer.setvolume(int(current * (1 - percentage)))
 
 
 if __name__ == "__main__":
+    mixer = aa.Mixer(cardindex=settings.MIXER_CARD, control=settings.MIXER_CONTROL)
+
     now = dt.datetime.now(tz=pytz.timezone(location.timezone))
     data = sun(location.observer)
 
-    for key, value in data.items():
-        print(now)
-        print(value)
-
-        if now > value:
-            print(key)
-            break
-
-    print(sun(location.observer))
+    attenuate(mixer)
--- audio/settings.py
+++ audio/settings.py
@@ -1,16 +1,31 @@
 from pathlib import Path
 
+import astral
 from decouple import config
 
 PROJECT_PATH = Path(__file__).parent.parent
 
 VOLUME_STEP = config("VOLUME_STEP", 5, int)
 
-CARD_ID = config("CARD_ID", 1, int)
+CARD_ID = config("CARD_ID", 0, int)
 DEVICE_ID = config("DEVICE_ID", "PCM,0")
 DEVICE_FANCY_ID = config("DEVICE_FANCY_ID", "sysdefault:CARD=Phone")
 
 SOUND_PATH = config("SOUND_PATH", PROJECT_PATH / "sounds", Path)
 SOUND_VOLUME_CHANGE = config(
     "SOUND_VOLUME_CHANGE", SOUND_PATH / "volume_change.wav", Path
+)
+
+MIXER_CONTROL = config("MIXER_CONTROL", "PCM")
+MIXER_CARD = config("MIXER_CARD", 0, cast=int)
+
+
+ASTRAL_CITY = config("ASTRAL_CITY", "Rome")
+ASTRAL_COUNTRY = config("ASTRAL_COUNTRY", "Italy")
+ASTRAL_TZ = config("ASTRAL_TZ", "Europe/Rome")
+ASTRAL_LATITUDE = config("ASTRAL_LATITUDE", 41.89193, cast=float)
+ASTRAL_LONGITUDE = config("ASTRAL_LONGITUDE", 12.51133, cast=float)
+
+ASTRAL_LOCATION = astral.LocationInfo(
+    ASTRAL_CITY, ASTRAL_COUNTRY, ASTRAL_TZ, ASTRAL_LATITUDE, ASTRAL_LONGITUDE
 )
